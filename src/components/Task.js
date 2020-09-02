65ebe40ca2950321434fdb7c027d778c31cb4d55
phaneendra <phaneendraandukuri@gmail.com>
2020-09-02 16:28:03 +0530

    changed hover in styling instead of keeping it in state

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,42 +1,19 @@
 import React from 'react';
 
-class Task extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hover: false };
-    this.toggleHover = this.toggleHover.bind(this);
-  }
-
-  toggleHover() {
-    this.setState({ hover: !this.state.hover });
-  }
-
-  render() {
-    const {
-      taskContent,
-      status,
-      toggleStatus,
-      taskId,
-      deleteTask,
-    } = this.props;
-    return (
-      <div
-        className={`task ${status}`}
-        onMouseOver={this.toggleHover}
-        onMouseOut={this.toggleHover}
-      >
-        <div className="task-item">
-          <p className="box"></p>
-          <p className="taskContent" onClick={() => toggleStatus(taskId)}>
-            {taskContent}
-          </p>
-        </div>
-        <p className="delete" onClick={() => deleteTask(taskId)}>
-          {this.state.hover ? 'x' : ''}
+const Task = ({ taskContent, status, toggleStatus, taskId, deleteTask }) => {
+  return (
+    <div className={`task ${status}`}>
+      <div className="task-item">
+        <p className="box"></p>
+        <p className="taskContent" onClick={() => toggleStatus(taskId)}>
+          {taskContent}
         </p>
       </div>
-    );
-  }
-}
+      <p className="delete" onClick={() => deleteTask(taskId)}>
+        x
+      </p>
+    </div>
+  );
+};
 
 export default Task;
