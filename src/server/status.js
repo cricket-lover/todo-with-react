4898f559157c70cb3f9f0358eafae6f7cd8ece4f
phaneendra <phaneendraandukuri@gmail.com>
2020-09-11 20:28:41 +0530

    separated server from frontend

--- src/server/status.js
+++
@@ -1,9 +0,0 @@
-const nextStatusList = { todo: 'doing', doing: 'done', done: 'todo' };
-
-const getDefaultStatus = () => 'todo';
-
-const getNextStatus = (currentStatus) => {
-  return nextStatusList[currentStatus];
-};
-
-module.exports = { getDefaultStatus, getNextStatus };
